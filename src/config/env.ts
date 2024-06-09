ea4776b28a26e8853236da3fef09a7bf4c167f9e
hotequil <hotequil@protonmail.com>
2024-06-09 09:45:51 -0300

    feat: make env file

---
+++ src/config/env.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export const env = {
+    webUrl: process.env.WEB_URL as string,
+    aiUrl: process.env.AI_URL as string,
+    azure: {
+        accountName: process.env.AZURE_ACCOUNT_NAME as string,
+        accountKey: process.env.AZURE_ACCOUNT_KEY as string,
+        storageContainerUrl: process.env.AZURE_STORAGE_CONTAINER_URL as string,
+    },
+    port: process.env.PORT as number,
+}
