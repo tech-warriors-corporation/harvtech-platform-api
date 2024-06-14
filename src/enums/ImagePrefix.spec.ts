92536ee2cd3526a4b620580c89345988c40379e2
hotequil <hotequil@protonmail.com>
2024-06-14 20:50:20 -0300

    test: make value enum for ImagePrefix

---
+++ src/enums/ImagePrefix.spec.ts
@@ -0,0 +1,7 @@
+import { ImagePrefix } from './ImagePrefix'
+
+describe('ImagePrefix', () => {
+    it('Should have a value in enum', () => {
+        expect(ImagePrefix.RICE_LEAF).toBeTruthy()
+    })
+})
