dc9dcfe612ce41723ebdf15c63972c17a8edc9f6
hotequil <hotequil@protonmail.com>
2024-06-15 15:15:01 -0300

    test: check if getImagePrefixFromModelType returns correct or incorrect prefix

---
+++ src/helpers/ImagePrefixHelper.spec.ts
@@ -0,0 +1,16 @@
+import { ImagePrefixHelper } from './ImagePrefixHelper'
+
+import { ImagePrefix } from '~enums/ImagePrefix'
+import { ModelType } from '~enums/ModelType'
+
+describe('ImagePrefixHelper', () => {
+    describe('getImagePrefixFromModelType', () => {
+        it('Should return the correct prefix for ModelType.RICE_LEAF', () => {
+            expect(ImagePrefixHelper.getImagePrefixFromModelType(ModelType.RICE_LEAF)).toBe(ImagePrefix.RICE_LEAF)
+        })
+
+        it('Should return nothing for an invalid ModelType', () => {
+            expect(ImagePrefixHelper.getImagePrefixFromModelType('INVALID_MODEl_TYPE' as ModelType)).toBeFalsy()
+        })
+    })
+})
