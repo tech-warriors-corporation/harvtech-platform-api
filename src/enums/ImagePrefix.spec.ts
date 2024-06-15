6a01018b98774a3e554602187db4b40e54ec6b39
hotequil <hotequil@protonmail.com>
2024-06-14 22:41:15 -0300

    test: make MIME types checks and remove enum tests

--- src/enums/ImagePrefix.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ImagePrefix } from './ImagePrefix'
-
-describe('ImagePrefix', () => {
-    it('Should have a value in enum', () => {
-        expect(ImagePrefix.RICE_LEAF).toBeTruthy()
-    })
-})
