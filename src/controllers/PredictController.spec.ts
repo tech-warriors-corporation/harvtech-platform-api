99c2a562c791a02b006fb9661badd94fb1bded16
hotequil <hotequil@protonmail.com>
2024-06-21 20:54:55 -0300

    test: add verification for Axios error

--- src/controllers/PredictController.spec.ts
+++ src/controllers/PredictController.spec.ts
@@ -1,4 +1,4 @@
-import { HttpStatusCode } from 'axios'
+import { AxiosError, HttpStatusCode } from 'axios'
 
 import { PredictController } from '~controllers/PredictController'
 import { ModelType } from '~enums/ModelType'
@@ -63,103 +63,116 @@ describe('PredictController', () => {
         }
     })
 
-    it(`Should return status ${HttpStatusCode.Ok} and text on successful image prediction`, async () => {
-        await controller.image(ctx)
+    describe('image', () => {
+        it(`Should return status ${HttpStatusCode.Ok} and text on successful image prediction`, async () => {
+            await controller.image(ctx)
 
-        expect(ctx.body.text).toBe('text')
-        expect(ctx.status).toBe(HttpStatusCode.Ok)
-    })
+            expect(ctx.body.text).toBe('text')
+            expect(ctx.status).toBe(HttpStatusCode.Ok)
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and model type required error message if modelType is missing`, async () => {
-        ctx.request.body.modelType = null
+        it(`Should return status ${HttpStatusCode.BadRequest} and model type required error message if modelType is missing`, async () => {
+            ctx.request.body.modelType = null
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.MODEL_TYPE_REQUIRED)
-    })
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.MODEL_TYPE_REQUIRED)
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and file required error message if file is missing`, async () => {
-        ctx.request.body.file = null
+        it(`Should return status ${HttpStatusCode.BadRequest} and file required error message if file is missing`, async () => {
+            ctx.request.body.file = null
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.FILE_REQUIRED)
-    })
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.FILE_REQUIRED)
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and file content not provided error message if file content is not provided`, async () => {
-        ctx.request.body.file.content = null
+        it(`Should return status ${HttpStatusCode.BadRequest} and file content not provided error message if file content is not provided`, async () => {
+            ctx.request.body.file.content = null
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.FILE_CONTENT_REQUIRED)
-    })
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.FILE_CONTENT_REQUIRED)
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and file type not provided error message if file type is not provided`, async () => {
-        ctx.request.body.file.type = null
+        it(`Should return status ${HttpStatusCode.BadRequest} and file type not provided error message if file type is not provided`, async () => {
+            ctx.request.body.file.type = null
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.FILE_TYPE_REQUIRED)
-    })
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.FILE_TYPE_REQUIRED)
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and invalid file type error message if file type is invalid`, async () => {
-        const type = 'INVALID_FILE_TYPE'
+        it(`Should return status ${HttpStatusCode.BadRequest} and invalid file type error message if file type is invalid`, async () => {
+            const type = 'INVALID_FILE_TYPE'
 
-        ctx.request.body.file.type = type
+            ctx.request.body.file.type = type
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.INVALID_FILE_TYPE.replace('type', type))
-    })
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.INVALID_FILE_TYPE.replace('type', type))
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and prefix error message if prefix is not found`, async () => {
-        const getImagePrefixFromModelType = ImagePrefixHelper.getImagePrefixFromModelType as jest.Mock
+        it(`Should return status ${HttpStatusCode.BadRequest} and prefix error message if prefix is not found`, async () => {
+            const getImagePrefixFromModelType = ImagePrefixHelper.getImagePrefixFromModelType as jest.Mock
 
-        getImagePrefixFromModelType.mockReturnValue('')
+            getImagePrefixFromModelType.mockReturnValue('')
 
-        ctx.request.body.modelType = 'INVALID_MODEL_TYPE' as ModelType
+            ctx.request.body.modelType = 'INVALID_MODEL_TYPE' as ModelType
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.PREFIX_NOT_FOUND)
-    })
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.PREFIX_NOT_FOUND)
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and image upload error message if uploadImage returns nothing`, async () => {
-        const uploadImage = azureService.uploadImage as jest.Mock
+        it(`Should return status ${HttpStatusCode.BadRequest} and image upload error message if uploadImage returns nothing`, async () => {
+            const uploadImage = azureService.uploadImage as jest.Mock
 
-        uploadImage.mockReturnValue('')
+            uploadImage.mockReturnValue('')
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.IMAGE_UPLOAD_FAILED)
-    })
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.IMAGE_UPLOAD_FAILED)
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and image analysis error message if getPredictImage returns nothing`, async () => {
-        const getPredictImage = aiService.getPredictImage as jest.Mock
+        it(`Should return status ${HttpStatusCode.BadRequest} and image analysis error message if getPredictImage returns nothing`, async () => {
+            const getPredictImage = aiService.getPredictImage as jest.Mock
 
-        getPredictImage.mockReturnValue('')
+            getPredictImage.mockReturnValue('')
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.IMAGE_ANALYSIS_FAILED)
-    })
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.IMAGE_ANALYSIS_FAILED)
+        })
 
-    it(`Should return status ${HttpStatusCode.BadRequest} and general error message if getPredictImage rejects`, async () => {
-        const getPredictImage = aiService.getPredictImage as jest.Mock
+        it(`Should return status ${HttpStatusCode.BadRequest} and general error message if getPredictImage rejects`, async () => {
+            const getPredictImage = aiService.getPredictImage as jest.Mock
 
-        getPredictImage.mockRejectedValue('')
+            getPredictImage.mockRejectedValue('')
 
-        await controller.image(ctx)
+            await controller.image(ctx)
 
-        expect(ctx.status).toBe(HttpStatusCode.BadRequest)
-        expect(ctx.body.error.message).toBe(PredictImageError.GENERAL_ERROR)
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.GENERAL_ERROR)
+        })
+
+        it(`Should return status ${HttpStatusCode.BadRequest} and general error message if something throws an AxiosError`, async () => {
+            const getPredictImage = aiService.getPredictImage as jest.Mock
+
+            getPredictImage.mockRejectedValue(new AxiosError('error'))
+
+            await controller.image(ctx)
+
+            expect(ctx.status).toBe(HttpStatusCode.BadRequest)
+            expect(ctx.body.error.message).toBe(PredictImageError.GENERAL_ERROR)
+        })
     })
 })
