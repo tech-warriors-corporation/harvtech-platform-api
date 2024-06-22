4168e731271ffbecdc756d68ebec8c931b524f78
hotequil <hotequil@protonmail.com>
2024-06-21 21:34:48 -0300

    feat: config routes for /accounts

--- src/routes/account-routes.ts
+++ src/routes/account-routes.ts
@@ -1,12 +1,9 @@
-import { Context } from 'koa'
-
 import Router from '@koa/router'
-import { dataSource } from '~config/database'
-import { AccountEntity } from '~entities/AccountEntity'
+import { AccountController } from '~controllers/AccountController'
+import { CryptoHelper } from '~helpers/CryptoHelper'
 
 export default (router: Router) => {
-    router.get('/accounts', async (ctx: Context) => {
-        // TODO: return an object with array
-        ctx.body = await dataSource.manager.find(AccountEntity)
-    })
+    const accountsController = new AccountController(new CryptoHelper())
+
+    router.post('/accounts/login', accountsController.login.bind(accountsController))
 }
