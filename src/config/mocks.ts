0f1094611d27b4e90ffd68056a5caac7db6c7816
hotequil <hotequil@protonmail.com>
2024-07-12 21:03:07 -0300

    test: add mock for login and accessToken

--- src/config/mocks.ts
+++ src/config/mocks.ts
@@ -1,7 +1,11 @@
+import request from 'supertest'
+
 import { dataSource } from '~config/database'
+import { makeHandleRequest } from '~config/tests'
 import { AccountEntity } from '~entities/AccountEntity'
 import { AccountPlan } from '~enums/AccountPlan'
 import { AccountType } from '~enums/AccountType'
+import { CryptoHelper } from '~helpers/CryptoHelper'
 
 type AddAccountParams = {
     name: string
@@ -15,4 +19,35 @@ export const mockAddAccount = async (params: AddAccountParams) => {
     const repository = await dataSource.getRepository(AccountEntity)
 
     await repository.save(params)
+}
+
+type LoginModel = {
+    email: string
+    password: string
+}
+
+export const mockLogin = async (model: LoginModel) => {
+    const cryptoHelper = new CryptoHelper()
+
+    await mockAddAccount({
+        name: 'HarvTech',
+        email: model.email,
+        password: cryptoHelper.encrypt(model.password),
+        type: AccountType.ADMIN,
+        plan: AccountPlan.DELUXE,
+    })
+
+    const { body, statusCode } = await request(makeHandleRequest()).post('/accounts/login').send(model)
+
+    return { body, statusCode }
+}
+
+type AccessTokenModel = LoginModel
+
+export const mockAccessToken = async (model: AccessTokenModel) => {
+    const {
+        body: { accessToken },
+    } = await mockLogin(model)
+
+    return `Bearer ${accessToken}`
 }
