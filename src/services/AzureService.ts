e5ccd8216190cd8a87956c35e5771263a2df40c2
hotequil <hotequil@protonmail.com>
2024-09-02 23:34:20 -0300

    feat: create generateFileNameUrl

--- src/services/AzureService.ts
+++ src/services/AzureService.ts
@@ -1,9 +1,15 @@
-import { ContainerClient, StorageSharedKeyCredential } from '@azure/storage-blob'
+import {
+    BlobSASPermissions,
+    ContainerClient,
+    generateBlobSASQueryParameters,
+    StorageSharedKeyCredential,
+} from '@azure/storage-blob'
 import { env } from '~config/env'
 
 export class AzureService {
     private readonly credential: StorageSharedKeyCredential
     private readonly containerClient: ContainerClient
+    private readonly FIFTEEN_MINUTES = 900000
 
     constructor() {
         this.credential = new StorageSharedKeyCredential(env.azure.accountName, env.azure.accountKey)
@@ -15,6 +21,20 @@ export class AzureService {
             blobHTTPHeaders: { blobContentType: contentType },
         })
 
-        return this.containerClient.getBlobClient(fileName).url
+        return this.generateFileNameUrl(fileName)
+    }
+
+    private generateFileNameUrl(fileName: string): string {
+        const blobClient = this.containerClient.getBlobClient(fileName)
+        const blobSasValues = {
+            containerName: this.containerClient.containerName,
+            blobName: fileName,
+            expiresOn: new Date(new Date().getTime() + this.FIFTEEN_MINUTES),
+            permissions: BlobSASPermissions.parse('r'),
+        }
+
+        const token = generateBlobSASQueryParameters(blobSasValues, this.credential).toString()
+
+        return `${blobClient.url}?${token}`
     }
 }
